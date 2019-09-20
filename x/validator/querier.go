83c948a76fec16037ae256b5e999f82ba4e3836a
Xuefeng Zhu <xzhu15@illinois.edu>
2019-09-20 13:04:55 -0700

    BE-300: Be able to query delegator and candidate

--- x/validator/querier.go
+++ x/validator/querier.go
@@ -1,6 +1,8 @@
 package validator
 
 import (
+	"fmt"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
@@ -12,6 +14,10 @@ func NewQuerier(keeper Keeper) sdk.Querier {
 		switch path[0] {
 		case QueryPuller:
 			return queryPuller(ctx, req, keeper)
+		case QueryPusher:
+			return queryPusher(ctx, req, keeper)
+		case QueryDelegator:
+			return queryDelegator(ctx, req, keeper)
 		default:
 			return nil, sdk.ErrUnknownRequest("Unknown validator query endpoint")
 		}
@@ -33,6 +39,50 @@ func queryPusher(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) ([]byte,
 	res, err := codec.MarshalJSONIndent(keeper.cdc, pusher)
 	if err != nil {
 		return nil, sdk.ErrInternal(sdk.AppendMsgToErr("Could not marshal result to JSON", err.Error()))
+	}
+
+	return res, nil
+}
+
+func queryDelegator(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) ([]byte, sdk.Error) {
+	var params QueryDelegatorParams
+	err := ModuleCdc.UnmarshalJSON(req.Data, &params)
+	if err != nil {
+		return nil, sdk.ErrInternal(fmt.Sprintf("Failed to parse params: %s", err))
+	}
+
+	delegator := keeper.GetDelegator(ctx, params.CandidateAddress, params.DelegatorAddress)
+	res, err := codec.MarshalJSONIndent(keeper.cdc, delegator)
+	if err != nil {
+		return nil, sdk.ErrInternal(sdk.AppendMsgToErr("Could not marshal result to JSON", err.Error()))
+
+	}
+
+	return res, nil
+}
+
+func queryCandidate(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) ([]byte, sdk.Error) {
+	var params QueryCandidateParams
+	err := ModuleCdc.UnmarshalJSON(req.Data, &params)
+	if err != nil {
+		return nil, sdk.ErrInternal(fmt.Sprintf("Failed to parse params: %s", err))
+	}
+
+	var candidate Candidate
+	var found bool
+	if params.Seq <= 0 {
+		candidate = keeper.GetCandidate(ctx, params.CandidateAddress)
+	} else {
+		candidate, found = keeper.GetCandidateSnapshot(ctx, params.CandidateAddress, sdk.NewInt(params.Seq))
+		if !found {
+			return nil, sdk.ErrInternal("Could not find corresponding candidate")
+		}
+	}
+
+	res, err := codec.MarshalJSONIndent(keeper.cdc, candidate)
+	if err != nil {
+		return nil, sdk.ErrInternal(sdk.AppendMsgToErr("Could not marshal result to JSON", err.Error()))
+
 	}
 
 	return res, nil
