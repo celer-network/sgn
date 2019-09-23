c683cf34fb78ae869aea0e36a3f218e98486e4b2
Xuefeng Zhu <xzhu15@illinois.edu>
2019-09-23 11:43:13 -0700

    BE-317: Handle withdraw msg

--- x/validator/types/key.go
+++ x/validator/types/key.go
@@ -1,7 +1,5 @@
 package types
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
-
 const (
 	// module name
 	ModuleName = "validator"
@@ -10,14 +8,16 @@ const (
 	StoreKey = ModuleName
 )
 
+const (
+	AttributeKeyEthAddress = "eth_address"
+)
+
 var (
-	PullerKey = []byte{0x01} // key for puller
-	PusherKey = []byte{0x02} // key for pusher
-
+	PullerKey          = []byte{0x01} // key for puller
+	PusherKey          = []byte{0x02} // key for pusher
 	DelegatorKeyPrefix = []byte{0x03} // Key prefix for delegator
-
-	CandidateKeyPrefix         = []byte{0x41} // Key prefix for candidate
-	CandidateSnapshotKeyPrefix = []byte{0x42} // Key prefix for candidate snapshot
+	CandidateKeyPrefix = []byte{0x04} // Key prefix for candidate
+	RewardKeyPrefix    = []byte{0x05} // Key prefix for reward
 )
 
 // get delegators key from candidate address
@@ -35,12 +35,7 @@ func GetCandidateKey(candidateAddr string) []byte {
 	return append(CandidateKeyPrefix, []byte(candidateAddr)...)
 }
 
-// get candidate snapshot key for candidate with candidateAddr and seq
-func GetCandidateSnapshotsKey(candidateAddr string) []byte {
-	return append(CandidateSnapshotKeyPrefix, []byte(candidateAddr)...)
-}
-
-// get candidate snapshot key for candidate with candidateAddr and seq
-func GetCandidateSnapshotKey(candidateAddr string, seq sdk.Int) []byte {
-	return append(GetCandidateSnapshotsKey(candidateAddr), seq.BigInt().Bytes()...)
+// get reward key from ethAddr
+func GetRewardKey(ethAddr string) []byte {
+	return append(RewardKeyPrefix, []byte(ethAddr)...)
 }
