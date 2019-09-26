efcf8c39c704e7f2200ad6ffb4d2badd40ef1256
Xuefeng Zhu <xzhu15@illinois.edu>
2019-09-26 13:25:05 -0700

    minor fix

--- x/subscribe/module.go
+++ x/subscribe/module.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 
 	"github.com/celer-network/sgn/x/subscribe/client/cli"
+	"github.com/celer-network/sgn/x/subscribe/client/rest"
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -48,6 +49,7 @@ func (AppModuleBasic) ValidateGenesis(bz json.RawMessage) error {
 
 // Register rest routes
 func (AppModuleBasic) RegisterRESTRoutes(ctx context.CLIContext, rtr *mux.Router) {
+	rest.RegisterRoutes(ctx, rtr)
 }
 
 // Get the root query command of this module
