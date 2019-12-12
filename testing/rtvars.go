13affec01b41c4e43160ed0d9c5c51e770827533
Changfeng Liu <steven24liu@gmail.com>
2019-12-11 16:06:25 -0800

    rename

--- testing/rtvars.go
+++ testing/rtvars.go
@@ -2,8 +2,13 @@ package testing
 
 import "github.com/celer-network/sgn/mainchain"
 
+// runtime variables, will be initialized before each test
 var (
 	// used by setup_mainchain and tests
 	Client0Addr = mainchain.Hex2Addr(Client0AddrStr)
 	Client1Addr = mainchain.Hex2Addr(Client1AddrStr)
+
+	// e2eProfile will be updated and used for each test
+	// not support parallel tests
+	E2eProfile *TestProfile
 )
