ca3f61d5b7592ccefa7d881a270f8d75601f8f84
Xuefeng Zhu <xzhu15@illinois.edu>
2019-12-30 18:28:21 -0800

    address comment

--- test/e2e/singlenode/setup_sgn.go
+++ test/e2e/singlenode/setup_sgn.go
@@ -68,6 +68,13 @@ func installSgn() error {
 		return err
 	}
 
+	cmd = exec.Command("cp", "./test/config/local_config.json", "./config.json")
+	// set cmd.Dir under repo root path
+	cmd.Dir, _ = filepath.Abs("../../..")
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
