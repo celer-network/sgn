7af314b642d9aeb07bc516b6dfa915d1eab83a77
Xuefeng Zhu <frankzhu94@gmail.com>
2020-01-24 17:05:57 -0600

    Trigger process when sync block (#145)

--- monitor/common.go
+++ monitor/common.go
@@ -38,10 +38,6 @@ func (m *EthMonitor) isPullerOrOwner(candidate mainchain.Addr) bool {
 // Is the current node the guard to submit state proof
 func (m *EthMonitor) isRequestGuard(request subscribe.Request, latestBlockNum uint64, eventBlockNumber uint64) bool {
 	requestGuards := request.RequestGuards
-	// FIX: temprary fix
-	if latestBlockNum < eventBlockNumber {
-		return false
-	}
 	blockNumberDiff := latestBlockNum - eventBlockNumber
 	guardIndex := uint64(len(requestGuards)+1) * blockNumberDiff / request.DisputeTimeout
 
