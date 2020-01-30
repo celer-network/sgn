9710d40d933fbf05dc46e1a2cead117a2880814e
Xuefeng Zhu <xzhu15@illinois.edu>
2020-01-30 10:44:49 -0800

    add gateway cors support

--- testing/osp/routes.go
+++ testing/osp/routes.go
@@ -15,12 +15,12 @@ func (rs *RestServer) registerRoutes() {
 	rs.Mux.HandleFunc(
 		"/requestGuard",
 		postRequestGuardHandlerFn(rs),
-	).Methods("POST")
+	).Methods(http.MethodPost)
 
 	rs.Mux.HandleFunc(
 		"/intendSettle",
 		postIntendSettleHandlerFn(rs),
-	).Methods("POST")
+	).Methods(http.MethodPost)
 }
 
 type (
