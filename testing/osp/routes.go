7521f08e0d60e2baa0d8c54f9d96ffccc2b550e9
Xuefeng Zhu <xzhu15@illinois.edu>
2020-01-30 10:28:11 -0800

    Add osp cmd gateway support

--- testing/osp/routes.go
+++ testing/osp/routes.go
@@ -1,9 +1,12 @@
 package osp
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 
 	"github.com/celer-network/goutils/log"
+	"github.com/celer-network/sgn/mainchain"
 	"github.com/celer-network/sgn/proto/chain"
 	tf "github.com/celer-network/sgn/testing"
 	"github.com/celer-network/sgn/x/subscribe"
@@ -36,7 +39,7 @@ type (
 func postRequestGuardHandlerFn(rs *RestServer) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req RequestGuardRequest
-		if !rest.ReadRESTReq(w, r, rs.transactor.CliCtx.Codec, &req) {
+		if !rest.ReadRESTReq(w, r, rs.cdc, &req) {
 			return
 		}
 
@@ -51,8 +54,24 @@ func postRequestGuardHandlerFn(rs *RestServer) http.HandlerFunc {
 			log.Errorln("could not marshal SignedSimplexState:", err)
 			return
 		}
-		msgRequestGuard := subscribe.NewMsgRequestGuard(rs.user.Address.Hex(), signedSimplexStateBytes, rs.transactor.Key.GetAddress())
-		rs.transactor.AddTxMsg(msgRequestGuard)
+
+		if rs.gateway == "" {
+			msgRequestGuard := subscribe.NewMsgRequestGuard(rs.user.Address.Hex(), signedSimplexStateBytes, rs.transactor.Key.GetAddress())
+			rs.transactor.AddTxMsg(msgRequestGuard)
+		} else {
+			reqBody, err := json.Marshal(map[string]string{
+				"ethAddr":                 rs.user.Address.Hex(),
+				"signedSimplexStateBytes": mainchain.Bytes2Hex(signedSimplexStateBytes),
+			})
+			if err != nil {
+				return
+			}
+			_, err = http.Post(rs.gateway+"/subscribe/request",
+				"application/json", bytes.NewBuffer(reqBody))
+			if err != nil {
+				return
+			}
+		}
 
 		w.Header().Set("Content-Type", "text/plain")
 		if _, err := w.Write([]byte("success")); err != nil {
@@ -64,7 +83,7 @@ func postRequestGuardHandlerFn(rs *RestServer) http.HandlerFunc {
 func postIntendSettleHandlerFn(rs *RestServer) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req IntendSettleRequest
-		if !rest.ReadRESTReq(w, r, rs.transactor.CliCtx.Codec, &req) {
+		if !rest.ReadRESTReq(w, r, rs.cdc, &req) {
 			return
 		}
 
