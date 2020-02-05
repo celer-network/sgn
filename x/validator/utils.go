578f0eefda7a5720c4b39c0323e696ded6a07e1f
Xuefeng Zhu <frankzhu94@gmail.com>
2020-02-04 17:51:32 -0800

    update single node test transactor (#160)
    * update single node test transactor
    
    * update multinode setup

--- x/validator/utils.go
+++ x/validator/utils.go
@@ -3,6 +3,7 @@ package validator
 import (
 	"math/big"
 
+	"github.com/celer-network/goutils/log"
 	"github.com/celer-network/sgn/mainchain"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -12,4 +13,13 @@ func GetCandidateInfoFromMainchain(ctx sdk.Context, keeper Keeper, ethAddress st
 	return keeper.ethClient.Guard.GetCandidateInfo(&bind.CallOpts{
 		BlockNumber: new(big.Int).SetUint64(keeper.globalKeeper.GetSecureBlockNum(ctx)),
 	}, mainchain.Hex2Addr(ethAddress))
+}
+
+func InitAccount(ctx sdk.Context, keeper Keeper, accAddress sdk.AccAddress) {
+	account := keeper.accountKeeper.GetAccount(ctx, accAddress)
+	if account == nil {
+		log.Infof("Set new account %x", accAddress)
+		account = keeper.accountKeeper.NewAccountWithAddress(ctx, accAddress)
+		keeper.accountKeeper.SetAccount(ctx, account)
+	}
 }
