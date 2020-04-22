ac7816c57cf07b9c5a95f81570cc7f219796b950
Xuefeng Zhu <xzhu15@illinois.edu>
2020-04-22 10:51:08 -0700

    add missing file

---
+++ x/sync/keeper/apply.go
@@ -0,0 +1,28 @@
+package keeper
+
+import (
+	"errors"
+
+	"github.com/celer-network/sgn/x/global"
+	"github.com/celer-network/sgn/x/sync/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (keeper Keeper) ApplyChange(ctx sdk.Context, change types.Change) error {
+	switch change.Type {
+	case types.SyncBlock:
+		return keeper.SyncBlock(ctx, change.Data)
+	default:
+		return errors.New("Invalid change type")
+	}
+
+	return nil
+}
+
+func (keeper Keeper) SyncBlock(ctx sdk.Context, data []byte) error {
+	var block global.Block
+	keeper.cdc.MustUnmarshalBinaryBare(data, &block)
+
+	keeper.gk.SyncBlock(ctx, block.Number)
+	return nil
+}
