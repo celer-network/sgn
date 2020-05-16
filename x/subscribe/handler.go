244a1fee798f4acb2cd8021b147c7a946d9131a8
Xuefeng Zhu <xzhu15@illinois.edu>
2020-05-15 18:20:04 -0700

    fix vet

--- x/subscribe/handler.go
+++ x/subscribe/handler.go
@@ -1,7 +1,6 @@
 package subscribe
 
 import (
-	"github.com/celer-network/sgn/seal"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
@@ -9,19 +8,9 @@ import (
 // NewHandler returns a handler for "subscribe" type messages.
 func NewHandler(keeper Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
-		logEntry := seal.NewMsgLog()
-		var res *sdk.Result
-		var err error
 		switch msg := msg.(type) {
 		default:
 			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unrecognized %s message type: %T", ModuleName, msg)
 		}
-
-		if err != nil {
-			logEntry.Error = append(logEntry.Error, err.Error())
-		}
-
-		seal.CommitMsgLog(logEntry)
-		return res, err
 	}
 }
