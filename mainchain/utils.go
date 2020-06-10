329ab977d65f97175de6badf7db5a71d3430c32c
Michael Zhou <zhoumotongxue008@gmail.com>
2020-06-10 13:42:27 +0800

    Use goutils/eth (#241)

--- mainchain/utils.go
+++ mainchain/utils.go
@@ -2,14 +2,9 @@ package mainchain
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"math/big"
-	"time"
 
-	"github.com/celer-network/goutils/log"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/ethereum/go-ethereum"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -35,71 +30,6 @@ func ParseStatus(dposCandidateInfo DPoSCandidateInfo) sdk.BondStatus {
 // GetEventSignature accepts the string of an event signature and return the hex
 func GetEventSignature(eventSigStr string) HashType {
 	return crypto.Keccak256Hash([]byte(eventSigStr))
-}
-
-func WaitMined(ctx context.Context, ec *ethclient.Client,
-	tx *ethtypes.Transaction, blockDelay uint64) (*ethtypes.Receipt, error) {
-	return WaitMinedWithTxHash(ctx, ec, tx.Hash().Hex(), blockDelay)
-}
-
-// WaitMined waits for tx to be mined on the blockchain
-// It returns tx receipt when the tx has been mined and enough block confirmations have passed
-func WaitMinedWithTxHash(ctx context.Context, ec *ethclient.Client,
-	txHash string, blockDelay uint64) (*ethtypes.Receipt, error) {
-	// an error possibly returned when a transaction is pending
-	const missingFieldErr = "missing required field 'transactionHash' for Log"
-
-	if ec == nil {
-		return nil, errors.New("nil ethclient")
-	}
-	queryTicker := time.NewTicker(time.Second)
-	defer queryTicker.Stop()
-	// wait tx to be mined
-	txHashBytes := Hex2Hash(txHash)
-	for {
-		receipt, rerr := ec.TransactionReceipt(ctx, txHashBytes)
-		if rerr == nil {
-			log.Debugf("Transaction mined. Waiting for %d block confirmations", blockDelay)
-			if blockDelay == 0 {
-				return receipt, rerr
-			}
-			break
-		} else if rerr == ethereum.NotFound || rerr.Error() == missingFieldErr {
-			// Wait for the next round
-			select {
-			case <-ctx.Done():
-				return nil, ctx.Err()
-			case <-queryTicker.C:
-			}
-		} else {
-			return receipt, rerr
-		}
-	}
-	// wait for enough block confirmations
-	ddl := big.NewInt(0)
-	latestBlockHeader, err := ec.HeaderByNumber(ctx, nil)
-	if err == nil {
-		ddl.Add(new(big.Int).SetUint64(blockDelay), latestBlockHeader.Number)
-	}
-	for {
-		latestBlockHeader, err := ec.HeaderByNumber(ctx, nil)
-		if err == nil && ddl.Cmp(latestBlockHeader.Number) < 0 {
-			receipt, rerr := ec.TransactionReceipt(ctx, txHashBytes)
-			if rerr == nil {
-				log.Debugln("tx confirmed!")
-				return receipt, rerr
-			} else if rerr == ethereum.NotFound || rerr.Error() == missingFieldErr {
-				return nil, errors.New("tx is dropped due to chain re-org")
-			} else {
-				return receipt, rerr
-			}
-		}
-		select {
-		case <-ctx.Done():
-			return nil, ctx.Err()
-		case <-queryTicker.C:
-		}
-	}
 }
 
 // GetTxSender returns the sender address of the given transaction
