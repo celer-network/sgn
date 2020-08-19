998cca564dcdf55d9be5012c1578c78150c699c1
Xuefeng Zhu <xzhu15@illinois.edu>
2020-08-19 00:00:20 -0700

    fix test

--- testing/common/constants.go
+++ testing/common/constants.go
@@ -13,7 +13,7 @@ const (
 	SgnChainID    = "sgntest"
 	SgnPassphrase = "12341234"
 	SgnGasPrice   = ""
-	SgnCLIAddr    = "cosmos1sev3nak38elq95lumnh6t2drjtfx73274vnnjh"
+	SgnCLIAddr    = "sgn1sev3nak38elq95lumnh6t2drjtfx7327upp6p2"
 	SgnNodeURI    = "tcp://localhost:26657"
 
 	SgnBlockInterval = 1
@@ -29,7 +29,7 @@ const (
 
 var (
 	SgnCLIHomes    = [...]string{"../../../docker-volumes/node0/sgncli", "../../../docker-volumes/node1/sgncli", "../../../docker-volumes/node2/sgncli"}
-	SgnOperators   = [...]string{"cosmos1ddvpnk98da5hgzz8lf5y82gnsrhvu3jd3cukpp", "cosmos1lh8cr9p2a9dxtunte0sn3qmjkyksdh5yc5yxph", "cosmos122w97t8vsa3538fr3ylvz3hvuqxrgpnax8es8f"}
+	SgnOperators   = [...]string{"sgn1ddvpnk98da5hgzz8lf5y82gnsrhvu3jdc4wlju", "sgn1lh8cr9p2a9dxtunte0sn3qmjkyksdh5y3ek0j2", "sgn122w97t8vsa3538fr3ylvz3hvuqxrgpna02te55"}
 	ValEthKs       = [...]string{"../../keys/ethks0.json", "../../keys/ethks1.json", "../../keys/ethks2.json"}
 	ValEthAddrs    = [...]string{"6a6d2a97da1c453a4e099e8054865a0a59728863", "ba756d65a1a03f07d205749f35e2406e4a8522ad", "f25d8b54fad6e976eb9175659ae01481665a2254"}
 	ClientEthKs    = [...]string{"../../keys/ethks3.json", "../../keys/ethks4.json"}
