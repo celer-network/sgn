167e471e33de66f30bbfbcf6ffb503f9e507800b
Xiaozhou Li <xli@celer.network>
2020-09-03 12:04:11 -0700

    remove unused gateway tx interface

--- gateway/tx.go
+++ gateway/tx.go
@@ -28,21 +28,6 @@ func (rs *RestServer) registerTxRoutes() {
 		"/validator/withdrawReward",
 		postWithdrawRewardHandlerFn(rs),
 	).Methods(http.MethodPost, http.MethodOptions)
-
-	rs.Mux.HandleFunc(
-		"/guard/subscribe",
-		postSubscribeHandlerFn(rs),
-	).Methods(http.MethodPost, http.MethodOptions)
-
-	rs.Mux.HandleFunc(
-		"/validator/updateSidechainAddr",
-		postUpdateSidechainAddrHandlerFn(rs),
-	).Methods(http.MethodPost, http.MethodOptions)
-
-	rs.Mux.HandleFunc(
-		"/validator/syncDelegator",
-		postSyncDelegatorHandlerFn(rs),
-	).Methods(http.MethodPost, http.MethodOptions)
 }
 
 type (
@@ -204,73 +189,6 @@ func postWithdrawRewardHandlerFn(rs *RestServer) http.HandlerFunc {
 		}
 
 		msg := validator.NewMsgWithdrawReward(req.EthAddr, transactor.Key.GetAddress())
-		writeGenerateStdTxResponse(w, transactor, msg)
-	}
-}
-
-func postSubscribeHandlerFn(rs *RestServer) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var req SubscribeRequest
-		transactor := rs.transactorPool.GetTransactor()
-		if !rest.ReadRESTReq(w, r, transactor.CliCtx.Codec, &req) {
-			return
-		}
-
-		subscription := guard.NewSubscription(req.EthAddr)
-		deposit, ok := sdk.NewIntFromString(req.Amount)
-		if !ok {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, "Invalid deposit amount")
-			return
-		}
-
-		subscription.Deposit = deposit
-		subscriptionData := transactor.CliCtx.Codec.MustMarshalBinaryBare(subscription)
-		msg := sync.NewMsgSubmitChange(sync.Subscribe, subscriptionData, rs.ethClient, transactor.Key.GetAddress())
-		writeGenerateStdTxResponse(w, transactor, msg)
-	}
-}
-
-func postUpdateSidechainAddrHandlerFn(rs *RestServer) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var req UpdateSidechainAddrRequest
-		transactor := rs.transactorPool.GetTransactor()
-		if !rest.ReadRESTReq(w, r, transactor.CliCtx.Codec, &req) {
-			return
-		}
-
-		sidechainAddr, err := rs.sgnContract.SidechainAddrMap(&bind.CallOpts{}, mainchain.Hex2Addr(req.EthAddr))
-		if err != nil {
-			log.Errorln("Query sidechain address error:", err)
-			rest.WriteErrorResponse(w, http.StatusBadRequest, "Fail to query sidechain address")
-			return
-		}
-
-		candidate := validator.NewCandidate(req.EthAddr, sdk.AccAddress(sidechainAddr))
-		candidateData := transactor.CliCtx.Codec.MustMarshalBinaryBare(candidate)
-		msg := sync.NewMsgSubmitChange(sync.UpdateSidechainAddr, candidateData, rs.ethClient, transactor.Key.GetAddress())
-		writeGenerateStdTxResponse(w, transactor, msg)
-	}
-}
-
-func postSyncDelegatorHandlerFn(rs *RestServer) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var req SyncDelegatorRequest
-		transactor := rs.transactorPool.GetTransactor()
-		if !rest.ReadRESTReq(w, r, transactor.CliCtx.Codec, &req) {
-			return
-		}
-
-		di, err := rs.dposContract.GetDelegatorInfo(&bind.CallOpts{}, mainchain.Hex2Addr(req.CandidateAddress), mainchain.Hex2Addr(req.DelegatorAddress))
-		if err != nil {
-			log.Errorf("Failed to query delegator info: %s", err)
-			rest.WriteErrorResponse(w, http.StatusBadRequest, "Fail to query delegator info")
-			return
-		}
-
-		delegator := validator.NewDelegator(req.CandidateAddress, req.DelegatorAddress)
-		delegator.DelegatedStake = sdk.NewIntFromBigInt(di.DelegatedStake)
-		delegatorData := transactor.CliCtx.Codec.MustMarshalBinaryBare(delegator)
-		msg := sync.NewMsgSubmitChange(sync.SyncDelegator, delegatorData, rs.ethClient, transactor.Key.GetAddress())
 		writeGenerateStdTxResponse(w, transactor, msg)
 	}
 }
