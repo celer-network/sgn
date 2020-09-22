c4016dd54398e008a55c90fa405a74d19b28cbc5
Xuefeng Zhu <frankzhu94@gmail.com>
2020-09-22 00:21:01 -0700

    Add log for gov handler (#388) (#389)

--- x/gov/handler.go
+++ x/gov/handler.go
@@ -3,6 +3,7 @@ package gov
 import (
 	"fmt"
 
+	"github.com/celer-network/sgn/seal"
 	"github.com/celer-network/sgn/x/gov/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -11,34 +12,44 @@ import (
 // NewHandler creates an sdk.Handler for all the gov type messages
 func NewHandler(keeper Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		logEntry := seal.NewMsgLog()
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 		var res *sdk.Result
 		var err error
 
 		switch msg := msg.(type) {
 		case MsgDeposit:
-			res, err = handleMsgDeposit(ctx, keeper, msg)
+			res, err = handleMsgDeposit(ctx, keeper, msg, logEntry)
 
 		case MsgSubmitProposal:
-			res, err = handleMsgSubmitProposal(ctx, keeper, msg)
+			res, err = handleMsgSubmitProposal(ctx, keeper, msg, logEntry)
 
 		case MsgVote:
-			res, err = handleMsgVote(ctx, keeper, msg)
+			res, err = handleMsgVote(ctx, keeper, msg, logEntry)
 
 		default:
 			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unrecognized %s message type: %T", ModuleName, msg)
 		}
 
+		if err != nil {
+			logEntry.Error = append(logEntry.Error, err.Error())
+		}
+
+		seal.CommitMsgLog(logEntry)
 		return res, err
 	}
 }
 
-func handleMsgSubmitProposal(ctx sdk.Context, keeper Keeper, msg MsgSubmitProposal) (*sdk.Result, error) {
+func handleMsgSubmitProposal(ctx sdk.Context, keeper Keeper, msg MsgSubmitProposal, logEntry *seal.MsgLog) (*sdk.Result, error) {
+	logEntry.Type = msg.Type()
+	logEntry.Sender = msg.Proposer.String()
+
 	proposal, err := keeper.SubmitProposal(ctx, msg.Content)
 	if err != nil {
 		return nil, err
 	}
 
+	logEntry.Govern.ProposalId = proposal.ProposalID
 	votingStarted, err := keeper.AddDeposit(ctx, proposal.ProposalID, msg.Proposer, msg.InitialDeposit)
 	if err != nil {
 		return nil, err
@@ -66,7 +77,12 @@ func handleMsgSubmitProposal(ctx sdk.Context, keeper Keeper, msg MsgSubmitPropos
 	}, nil
 }
 
-func handleMsgDeposit(ctx sdk.Context, keeper Keeper, msg MsgDeposit) (*sdk.Result, error) {
+func handleMsgDeposit(ctx sdk.Context, keeper Keeper, msg MsgDeposit, logEntry *seal.MsgLog) (*sdk.Result, error) {
+	logEntry.Type = msg.Type()
+	logEntry.Sender = msg.Depositor.String()
+	logEntry.Govern.ProposalId = msg.ProposalID
+	logEntry.Govern.Amount = msg.Amount.Uint64()
+
 	votingStarted, err := keeper.AddDeposit(ctx, msg.ProposalID, msg.Depositor, msg.Amount)
 	if err != nil {
 		return nil, err
@@ -92,7 +108,12 @@ func handleMsgDeposit(ctx sdk.Context, keeper Keeper, msg MsgDeposit) (*sdk.Resu
 	return &sdk.Result{Events: ctx.EventManager().Events()}, nil
 }
 
-func handleMsgVote(ctx sdk.Context, keeper Keeper, msg MsgVote) (*sdk.Result, error) {
+func handleMsgVote(ctx sdk.Context, keeper Keeper, msg MsgVote, logEntry *seal.MsgLog) (*sdk.Result, error) {
+	logEntry.Type = msg.Type()
+	logEntry.Sender = msg.Voter.String()
+	logEntry.Govern.ProposalId = msg.ProposalID
+	logEntry.Govern.Option = uint32(msg.Option)
+
 	err := keeper.AddVote(ctx, msg.ProposalID, msg.Voter, msg.Option)
 	if err != nil {
 		return nil, err
