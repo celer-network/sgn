f6f08ec3a0602da8d66ae5799f4280d67f411437
Xiaozhou Li <xli@celer.network>
2020-09-23 14:09:20 -0700

    add MaxValidatorDiff param

--- x/sync/keeper/apply.go
+++ x/sync/keeper/apply.go
@@ -50,14 +50,15 @@ func (keeper Keeper) ConfirmParamProposal(ctx sdk.Context, change types.Change) 
 	log.Infoln("Apply confirm param proposal", paramChange)
 	switch paramChange.Record.Uint64() {
 	case mainchain.MaxValidatorNum:
+		maxValidatorDiff := keeper.validatorKeeper.MaxValidatorDiff(ctx)
 		ss, ok := keeper.paramsKeeper.GetSubspace(staking.DefaultParamspace)
 		if !ok {
 			return false, fmt.Errorf("Fail to get staking subspace")
 		}
 
-		// let sidechain maxValidator always larger than mainchain maxValdiator by 10,
+		// let sidechain maxValidator larger than mainchain maxValdiator
 		// to tolerate the latency for sidechain to keep in sync with mainchain.
-		maxValidator := paramChange.NewValue.Add(sdk.NewInt(10))
+		maxValidator := paramChange.NewValue.Add(sdk.NewInt(int64(maxValidatorDiff)))
 		err := ss.Update(ctx, staking.KeyMaxValidators, []byte(maxValidator.String()))
 		if err != nil {
 			return false, err
